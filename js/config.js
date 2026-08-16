e6161b25b3f41a7054e10636c4abd5c18aaa3e11
rivopolt <rivopoltimae@gmail.com>
2026-08-16 21:24:53 +0300

    Update apiUrl to new endpoint

--- js/config.js
+++ js/config.js
@@ -52,7 +52,7 @@ const CONFIG = {
      tühi, näitab rakendus salvestamisel selget veateadet.
   ------------------------------------------------------------------- */
   dataUrl: "data/registrations_all.geojson",
-  apiUrl: "",  // nt "https://jaljed-api.SINU-NIMI.workers.dev"
+  apiUrl: "https://jaljed-api.rivopolt.workers.dev",
 
   tracks: {
     species: [
